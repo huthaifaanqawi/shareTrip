5d7f7c4a66d16cf85134a6241c97ab1e2b237215
andres <andres@172.17.5.205>
2016-03-17 00:01:52 -0500

    member service

--- src/main/java/edu/mum/shareTrip/repository/MemberRepository.java
+++ src/main/java/edu/mum/shareTrip/repository/MemberRepository.java
@@ -1,11 +1,41 @@
 package edu.mum.shareTrip.repository;
 
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import edu.mum.shareTrip.domain.Member;
+import edu.mum.shareTrip.domain.Vechile;
 
 @Repository
 public interface MemberRepository extends CrudRepository<Member, Integer>{
 
+	@Query("SELECT m FROM MEMBER m, TRIP t where "
+	 		+ "m.id=:memberid "
+	 		+ "and t.member.id = m.id "
+	 		+ "and t.id !=:tripid "
+	 		+ "and ((t.departTime<=:min and t.returnTime>=:max) "
+	 		+ "or (t.returnTime>=:max and t.departTime<=:min))")
+	 public List<Member> getMemberAvailabilityTrip(@Param("memberid")long memberid, 
+			 @Param("min") Date mindate,
+			 @Param("max") Date maxdate,
+			 @Param("tripid")long idtrip);
+	 
+	 @Query("SELECT m FROM MEMBER m, TRIP t, BOOK b where "
+		 		+ "b.trip.id = t.id "
+		 		+ "and m.id=:memberid "
+		 		+ "and b.member.id = m.id "
+		 		+ "and ((t.departTime<=:min and t.returnTime>=:max) "
+		 		+ "or (t.returnTime>=:max and t.departTime<=:min))")
+	public List<Member> getMemberAvailabilityBook(@Param("memberid")long id, 
+				 @Param("min") Date mindate,
+				 @Param("max") Date maxdate);
+	 
+	@Query("SELECT m FROM MEMBER m where m.credentials.username=:name")
+	public Member getMemberByUserName(@Param("name")String username);
 }
--- src/main/java/edu/mum/shareTrip/service/MemberService.java
+++ src/main/java/edu/mum/shareTrip/service/MemberService.java
@@ -1,8 +1,18 @@
 package edu.mum.shareTrip.service;
 
+import java.util.Date;
+import java.util.List;
+
 import edu.mum.shareTrip.domain.Member;
 
 public interface MemberService {
 
 	public Member saveMember(Member member);
+	
+	public Member getMemberByUserName(String username);
+	
+	public List<Member> getMemberAvailabilityTrip(long id, Date mindate, Date maxdate, long tripid);
+	
+	public List<Member> getMemberAvailabilityBook(long id, Date mindate, Date maxdate);
+	
 }
